97bfb8f5333cfd42d17f126b0e8b3db98fd40c61
Rupam Das <rejisterjack@gmail.com>
2025-03-20 15:59:14 +0530

    feat: render video cards

--- src/main.js
+++ src/main.js
@@ -14,11 +14,40 @@ document.addEventListener("DOMContentLoaded", () => {
         data.data &&
         Array.isArray(data.data.data)
       ) {
-        console.log("Fetched videos:", data.data.data)
-        
+        videos = data.data.data.map((item) => {
+          const video = item.items
+          return {
+            videoId: video.id,
+            title: video.snippet.title,
+            channelName: video.snippet.channelTitle,
+            thumbnail: video.snippet.thumbnails.high.url,
+          }
+        })
+
+        displayVideos(videos)
       } else {
         console.error("Invalid API response format")
       }
     })
     .catch((error) => console.error("Error fetching videos:", error))
+
+  function displayVideos(videos) {
+    videoGrid.innerHTML = videos
+      .map(
+        (video) => `
+            <div class="col-md-4 mb-4">
+                <div class="card h-100 shadow-sm">
+                    <a href="https://www.youtube.com/watch?v=${video.videoId}" target="_blank">
+                        <img src="${video.thumbnail}" class="card-img-top" alt="${video.title}">
+                    </a>
+                    <div class="card-body">
+                        <h6 class="card-title">${video.title}</h6>
+                        <p class="card-text text-muted">Channel: ${video.channelName}</p>
+                    </div>
+                </div>
+            </div>
+        `
+      )
+      .join("")
+  }
 })
